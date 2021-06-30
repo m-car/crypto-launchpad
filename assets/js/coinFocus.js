d38b3529652c32e318f759b4046a9e82aad70007
Maxwell Dunn <max.dunn63@gmail.com>
2021-06-30 13:25:31 -0700

    Appended most needed data to page, needs error clause

--- assets/js/coinFocus.js
+++ assets/js/coinFocus.js
@@ -5,12 +5,34 @@ function fetchCoin(link) {
         return response.json();
     })
     .then(function(data){
-        console.log(data);
+            console.log(data);
+            appendData(data)
     })
 }
 
-function appendData() {
-    
+function appendData(data) {
+    var coinName = data.name;
+    var description = data.description.en.split('.')[0] + '.';
+    var price = data.market_data.current_price.usd;
+    var high24 = data.market_data.high_24h.usd;
+    var low24 = data.market_data.low_24h.usd;
+    var marketCap = data.market_data.market_cap.usd;
+    var priceChange1h = data.market_data.price_change_percentage_1h_in_currency.usd.toFixed(3);
+    var ath = data.market_data.ath.usd;
+    var atl = data.market_data.atl.usd;
+    var athChange = data.market_data.ath_change_percentage.usd.toFixed(3);
+    var atlChange = data.market_data.atl_change_percentage.usd.toFixed(3);
+    $('#coin-name').text(coinName);
+    $('#description').text(description);
+    $('#price').text("Current Price: " + price);
+    $('#high24').text("Highest in the last 24 hours: " + high24);
+    $('#low24').text("Lowest in the last 24 hours: " + low24);
+    $('#marketCap').text("Market Cap: " + marketCap);
+    $('#priceChange1h').text("Price Change(%) in the last hour: " + priceChange1h);
+    $('#ath').text("All time high: " + ath);
+    $('#athChange').text("All time low: " + atl);
+    $('#atl').text("All time high change(%): " + athChange);
+    $('#atlChange').text("All time low change(%): " + atlChange);
 }
 
 function getSearch() {
