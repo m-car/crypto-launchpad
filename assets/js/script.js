5f9d753ffeaa4a45876b533b21d73b90a4e3785d
Vivi Cowan <vivicare@gmail.com>
2021-07-01 12:46:57 -0700

    pulled popular coins onto homepage

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,10 +23,6 @@ fetch(cryptoAPI)
 })
 
 
-
-
-
-
 function getNews(search){
 
 var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + search + "&api-key="+ newsKEY;
@@ -80,7 +76,75 @@ $("#userInput").on('submit', function(event) {
 });
 
 $("#pullAllBtn").on('click', function() {
-    alert('this button works')
+    showPopularCurrencies();
     // Functions to pull all coins go here
 }); 
 
+var base_url = "https://api.coingecko.com/api/v3"
+var result = document.getElementById("result")
+
+function showPopularCurrencies(){
+    console.log("searching popular currencies")
+
+    var query = new URLSearchParams(location.search)
+    var page = query.get("page") || 1
+
+    var sort = document.getElementById("sort").value
+    var order = document.getElementById("order").value
+    
+    var params = new URLSearchParams()
+    params.append("vs_currency", "usd")
+    params.append("order", sort+order)
+    params.append("per_page", "50")
+    params.append("page", page)
+    params.append("price_chage_percentage", "24h")
+    
+    fetch(base_url+"/coins/markets?"+params)
+    .then(function(response){
+    console.log(response.status);
+    return response.json();
+    })
+    .then(function(data){
+        console.log(data); 
+        displayCoins(data);
+    })
+}
+
+function displayCoins(data){
+    var d = data
+
+    result.innerHTML = ""
+
+    d.forEach(function(cryptocurrency){
+        var div = createCard(cryptocurrency);
+        result.append(div)
+    })
+
+    function createCard(cryptocurrency){
+        var div = document.createElement("div");
+        div.setAttribute("class","card");
+        div.setAttribute("id",cryptocurrency["id"])
+        var img = document.createElement("img");
+        img.setAttribute("src", cryptocurrency.image)
+        var details = document.createElement("div");
+        details.innerText =  cryptocurrency.name
+
+        var price = document.createElement("div");
+        price.innerText = "$ "+Math.round(100*cryptocurrency.current_price)/100
+        var priceChange = document.createElement("div");
+        priceChange.innerText = "(" + Math.round(100*cryptocurrency.price_change_percentage_24h)/100 + "%)"
+        priceChange.style.fontSize = "13px"
+                
+        if (cryptocurrency.price_change_percentage_24h > 0) {
+            price.style.color = "green"
+            priceChange.style.color = "green"
+        }
+        else {
+            price.style.color = "red"
+            priceChange.style.color = "red"
+        }
+        div.append(img,details,price,priceChange);
+        return div
+
+    }
+}
