0981c59ef4a4ac7c0a6a5b1bf87bcaeb6a52ee83
Vivi Cowan <vivicare@gmail.com>
2021-07-01 10:08:17 -0700

    Chart for trending coin

---
+++ assets/js/chart.js
@@ -0,0 +1,78 @@
+window.addEventListener('load', setup);
+
+var trendingCoins = [];
+var randomCoin = "";
+
+function setup(){
+	fetch("https://api.coingecko.com/api/v3/search/trending")
+	.then(function(response){
+		console.log(response)
+		return response.json();
+	})
+	.then(function(trending){
+		console.log(trending);	
+		for( var i = 0; i < trending.coins.length; i++){
+			trendingCoins[i] = trending.coins[i].item.id;
+		}
+
+		randomCoin = trendingCoins[Math.floor(Math.random() * trending.coins.length)];
+
+		var cryptoAPI = "https://api.coingecko.com/api/v3/coins/" + randomCoin 
+		fetch(cryptoAPI)
+		.then(function(response){
+			console.log(response)
+			return response.json();
+		})
+		.then(function(feature){
+			console.log(feature);	
+
+			var randomColor = ["rgb(252,250,100)", "rgb(79,167,230)",];
+			var ctx = document.getElementById("myChart");
+			var myChart = new Chart(ctx,{
+				
+				type: 'bar',
+				data: {
+					labels: [randomCoin],
+					datasets: [
+						{
+							label: 'Market Cap',
+							data: [feature.market_data.market_cap.usd],
+							backgroundColor:"rgb(252,250,100)",
+							borderColor: "rgb(252,250,100)",
+							borderDash: [5, 5],
+						
+
+						}, {
+							label: 'Total Volume',
+							data: [feature.market_data.total_volume.usd],
+							backgroundColor: "rgb(79,167,230)",
+							borderColor: "rgb(79,167,230)", 
+							borderWidth: 4
+						}
+
+					]
+				},			
+			});
+		}) 
+	}) 
+}	
+	
+	
+	
+	
+	
+	
+	
+	
+
+
+
+
+
+
+
+
+
+
+			
+	
